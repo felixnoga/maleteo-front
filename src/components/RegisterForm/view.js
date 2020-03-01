a8e84696750f3ee73ec28ae2f26371107a77a7da
“Andres” <andres.bio360@gmail.com>
2020-03-02 00:19:49 +0100

    state de formulario creado y seteado con los valores introducidos por el usuario

--- src/components/RegisterForm/view.js
+++ src/components/RegisterForm/view.js
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './style.sass'
 
 function RegisterForm() {
+  const [registerForm, setRegisterForm] = useState({
+    email: '',
+    name: '',
+    surname: '',
+    password: '',
+    offerts: false
+  })
+
+  function handleEmail(e) {
+    const { value } = e.target
+    setRegisterForm({ ...registerForm, email: value })
+    console.log(registerForm)
+  }
+
+  function handleName(e) {
+    const { value } = e.target
+    setRegisterForm({ ...registerForm, name: value })
+  }
+
+  function handleSurname(e) {
+    const { value } = e.target
+    setRegisterForm({ ...registerForm, surname: value })
+  }
+
+  function handlePassword(e) {
+    const { value } = e.target
+    setRegisterForm({ ...registerForm, password: value })
+  }
+  function handleOfferts(e) {
+    let checkbox = document.getElementById('register_ofertas__checkbox')
+    let { offerts } = registerForm
+
+    if (checkbox.className === 'unchecked') {
+      checkbox.className = 'checked'
+      offerts = true
+    } else {
+      checkbox.className = 'unchecked'
+      offerts = false
+    }
+    console.log(offerts)
+    setRegisterForm({ ...registerForm, offerts: offerts })
+  }
+
+  //   console.log(registerForm)
+
   return (
     <div id="register_container">
       <h2>Únete a Maleteo y disfruta de sus ventajas</h2>
@@ -10,15 +55,30 @@ function RegisterForm() {
       <form>
         <label htmlFor="register_email">
           <p>Direccion de correo electrónico</p>
-          <input type="email" name="register_email" id="register_email" />
+          <input
+            type="email"
+            name="register_email"
+            id="register_email"
+            onChange={handleEmail}
+          />
         </label>
         <label htmlFor="register_name">
           <p>Nombre</p>
-          <input type="text" name="register_name" id="register_name" />
+          <input
+            type="text"
+            name="register_name"
+            id="register_name"
+            onChange={handleName}
+          />
         </label>
         <label htmlFor="register_surname">
           <p>Apellido</p>
-          <input type="text" name="register_surname" id="register_surname" />
+          <input
+            type="text"
+            name="register_surname"
+            id="register_surname"
+            onChange={handleSurname}
+          />
         </label>
         <label htmlFor="register_password">
           <p>Contraseña</p>
@@ -26,6 +86,7 @@ function RegisterForm() {
             type="password"
             name="register_password"
             id="register_password"
+            onChange={handlePassword}
           />
         </label>
         {/* 
@@ -33,12 +94,13 @@ function RegisterForm() {
 
         <div>
           <label htmlFor="register_ofertas__checkbox">
-            {' '}
             <input
               type="checkbox"
               name="register_ofertas__checkbox"
               id="register_ofertas__checkbox"
-            />{' '}
+              className="unchecked"
+              onClick={handleOfferts}
+            />
             Recibe Ofertas
           </label>
         </div>
