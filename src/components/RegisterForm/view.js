541ca8aa4eb5bc4c3ba35b3aae048f8e976579e1
“Andres” <andres.bio360@gmail.com>
2020-03-02 03:47:17 +0100

    validacion de clave agregada, nombre y apellido seteado (primera letra mayuscula, se puede incluir dos nombres y dos apellidos, no incluye espacios principio y fin)

--- src/components/RegisterForm/view.js
+++ src/components/RegisterForm/view.js
@@ -11,28 +11,88 @@ function RegisterForm() {
     offerts: false
   })
 
+  const [registerName, setRegisterName] = useState('')
+
+  const [registerSurname, setRegisterSurname] = useState('')
+
+  const PASSWORD_REGEXP = /^(?=.*?[0-9])(?=.*?[A-Z])(?=.*?[a-z]).{8,}$/
+
   function handleEmail(e) {
     const { value } = e.target
-    setRegisterForm({ ...registerForm, email: value })
-    console.log(registerForm)
+    setRegisterForm({ ...registerForm, email: value.trim() })
   }
 
   function handleName(e) {
-    const { value } = e.target
-    setRegisterForm({ ...registerForm, name: value })
+    const name = e.target.value.trim()
+
+    setRegisterName(name)
+
+    if (registerName) {
+      const space = registerName.indexOf(' ')
+
+      if (space > 0) {
+        const firstName = registerName.slice(0, space).trim()
+        const secondName = registerName.slice(space).trim()
+
+        const formFirstName =
+          firstName.charAt(0).toUpperCase() + firstName.slice(1)
+        const formSecondName =
+          secondName.charAt(0).toUpperCase() + secondName.slice(1)
+
+        const fullName = formFirstName.concat(' ', formSecondName)
+
+        setRegisterForm({ ...registerForm, name: fullName.trim() })
+      } else if (space === -1) {
+        const name =
+          registerName.charAt(0).toUpperCase() + registerName.slice(1)
+
+        setRegisterForm({ ...registerForm, name: name.trim() })
+      }
+    }
   }
 
   function handleSurname(e) {
-    const { value } = e.target
-    setRegisterForm({ ...registerForm, surname: value })
+    const surName = e.target.value.trim()
+
+    setRegisterSurname(surName)
+
+    if (registerSurname) {
+      const space = registerSurname.indexOf(' ')
+
+      if (space > 0) {
+        const firstSurname = registerSurname.slice(0, space).trim()
+        const secondSurname = registerSurname.slice(space).trim()
+
+        const formFirstSurname =
+          firstSurname.charAt(0).toUpperCase() + firstSurname.slice(1)
+        const formSecondSurname =
+          secondSurname.charAt(0).toUpperCase() + secondSurname.slice(1)
+
+        const fullSurname = formFirstSurname.concat(' ', formSecondSurname)
+
+        setRegisterForm({ ...registerForm, surname: fullSurname.trim() })
+      } else if (space === -1) {
+        const surname =
+          registerSurname.charAt(0).toUpperCase() + registerSurname.slice(1)
+
+        setRegisterForm({ ...registerForm, surname: surname.trim() })
+      }
+    }
   }
 
   function handlePassword(e) {
     const { value } = e.target
-    setRegisterForm({ ...registerForm, password: value })
+
+    if (!PASSWORD_REGEXP.test(value)) {
+      console.log('contraseña no valida')
+    } else {
+      console.log('contraseña valida')
+      setRegisterForm({ ...registerForm, password: value.trim() })
+    }
   }
+
   function handleOfferts(e) {
-    let checkbox = document.getElementById('register_ofertas__checkbox')
+    const checkbox = document.getElementById('register_ofertas__checkbox')
     let { offerts } = registerForm
 
     if (checkbox.className === 'unchecked') {
@@ -42,11 +102,10 @@ function RegisterForm() {
       checkbox.className = 'unchecked'
       offerts = false
     }
-    console.log(offerts)
     setRegisterForm({ ...registerForm, offerts: offerts })
   }
 
-  //   console.log(registerForm)
+  console.log(registerForm)
 
   return (
     <div id="register_container">
@@ -105,7 +164,16 @@ function RegisterForm() {
           </label>
         </div>
 
-        <button type="submit">Registrarse</button>
+        <button
+          disabled={
+            !registerForm.name ||
+            !registerForm.surname ||
+            !registerForm.password
+          }
+          type="submit"
+        >
+          Registrarse
+        </button>
       </form>
     </div>
   )
